0cee5fc58b45f29b64bd990868f0720032e4936e
Mateus Rocha <tateusgameplay@gmail.com>
2024-04-06 17:12:43 -0300

    add questions

--- main.cpp
+++ main.cpp
@@ -5,14 +5,88 @@
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 volatile byte start_state = LOW;
 
-String easyQuestions[5] = {
-    // TODO - Adicionar questões e respostas
-    "Arduino tem 14 portas digitais?",
+String easyQuestions[10] = {
+  // TODO - Adicionar questões e respostas
+  "Arduino tem 14 portas digitais?",
+  "Pedro Alvares Cabral descobriu o Brasil?",
+  "Baleia azul e o maior animal vivo no planeta?"
+  "A segunda guerra mundial acabou em 1945?"
+  "As 3 leis da física foi criado por Newton?"
+  "Windows parou de atualizar no 10?"
+  "A lingua mais falada no mundo e ingles?"
+  "O alfabeto tem 27 letras?"
+  "Homem aranha foi o primeiro heroi da marvel?"
+  "A FEI perdeu o RoboCup 2022?"
 };
-bool easyQuestionsResponse[5] = {};
+bool easyAnswers[10] = {
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  false,
+  false,
+  false,
+};
 
-String mediumQuestions[5] = {};
-String hardQuestions[5] = {};
+String mediumQuestions[10] = {
+  "A queda do muro de Berlim foi em 89?"
+  "Vargas tomou o poder em 1930?"
+  "O menor osso humano fica no ouvido?"
+  "Steve Jobs faleceu em 2011?"
+  "Arduino foi criado em 2005?"
+  "O primeiro computador e de 1930?"
+  "A revolucao industrial foi no sec.XX"
+  "Ayrton Senna morreu na Franca?"
+  "A FEI foi fundada em 1947?"
+  "O BR ganhou a copa do mundo em 98?"
+};
+bool mediumAnswers[10]{
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  false,
+  false,
+  false,
+}
+
+String hardQuestions[10] = {
+  "Steve Jobs foi CEO da Pixar?"
+  "Ada Lovelace foi a primeira programadora?"
+  "Ayrton Senna venceu 3 mundiais?"
+  "A boneca barbie foi lancada em 59?"
+  "O homem pisou na lua em 69?"
+  "O tupi guarani e uma lingua oficial do BR?"
+  "O Brasil tem 3 liguas oficiais?"
+  "Cinderela a primeira princesa da Disney?"
+  "Akira Toriyama faleceu dia 8 de marco de 2024?"
+  "Bambam durou 1 minuto contra Popo?"
+};
+bool hardAnswers[10]{
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  false,
+  false,
+  false,
+}
+
+String finalQuestion[3]{
+  "Chico moedas e um homem integro?"
+}
+bool finalAnswer[3]{
+  true,
+}
 bool gameActive = false;
 int questionIndex = 0;
 int score = 0;
