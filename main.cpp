a9c6c386ae6de27b774779cf0b429a2ec227b6d9
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-04-09 20:02:27 -0300

    testing print string to lcd

--- main.cpp
+++ main.cpp
@@ -5,92 +5,100 @@
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 volatile byte start_state = LOW;
 
-String easyQuestions[10] = {
-  // TODO - Adicionar questões e respostas
-  "Arduino tem 14 portas digitais?",
-  "Pedro Alvares Cabral descobriu o Brasil?",
-  "Baleia azul e o maior animal vivo no planeta?"
-  "A segunda guerra mundial acabou em 1945?"
-  "As 3 leis da física foi criado por Newton?"
-  "Windows parou de atualizar no 10?"
-  "A lingua mais falada no mundo e ingles?"
-  "O alfabeto tem 27 letras?"
-  "Homem aranha foi o primeiro heroi da marvel?"
-  "A FEI perdeu o RoboCup 2022?"
+const String easyQuestions[] = {
+    "Arduino tem 14 portas digitais?",
+    "Pedro Alvares Cabral descobriu o Brasil?",
+    "Baleia azul e o maior animal vivo no planeta?",
+    "A segunda guerra mundial acabou em 1945?",
+    "As 3 leis da física foi criado por Newton?",
+    "Windows parou de atualizar no 10?",
+    "A lingua mais falada no mundo e ingles?",
+    "O alfabeto tem 27 letras?",
+    "Homem aranha foi o primeiro heroi da marvel?",
+    "A FEI perdeu o RoboCup 2022?",
+};
+
+const char easyQuestionsTeste[][50] PROGMEM = {
+    "Arduino tem 14 portas digitais?",
+    "Pedro Alvares Cabral descobriu o Brasil?",
+    "Baleia azul e o maior animal vivo no planeta?",
+
 };
 bool easyAnswers[10] = {
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  false,
-  false,
-  false,
-  false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
 };
 
 String mediumQuestions[10] = {
-  "A queda do muro de Berlim foi em 89?"
-  "Vargas tomou o poder em 1930?"
-  "O menor osso humano fica no ouvido?"
-  "Steve Jobs faleceu em 2011?"
-  "Arduino foi criado em 2005?"
-  "O primeiro computador e de 1930?"
-  "A revolucao industrial foi no sec.XX"
-  "Ayrton Senna morreu na Franca?"
-  "A FEI foi fundada em 1947?"
-  "O BR ganhou a copa do mundo em 98?"
+    "A queda do muro de Berlim foi em 89?",
+    "Vargas tomou o poder em 1930?",
+    "O menor osso humano fica no ouvido?",
+    "Steve Jobs faleceu em 2011?",
+    "Arduino foi criado em 2005?",
+    "O primeiro computador e de 1930?",
+    "A revolucao industrial foi no sec.XX",
+    "Ayrton Senna morreu na Franca?",
+    "A FEI foi fundada em 1947?",
+    "O BR ganhou a copa do mundo em 98?",
 };
 bool mediumAnswers[10]{
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  false,
-  false,
-  false,
-  false,
-}
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+};
 
 String hardQuestions[10] = {
-  "Steve Jobs foi CEO da Pixar?"
-  "Ada Lovelace foi a primeira programadora?"
-  "Ayrton Senna venceu 3 mundiais?"
-  "A boneca barbie foi lancada em 59?"
-  "O homem pisou na lua em 69?"
-  "O tupi guarani e uma lingua oficial do BR?"
-  "O Brasil tem 3 liguas oficiais?"
-  "Cinderela a primeira princesa da Disney?"
-  "Akira Toriyama faleceu dia 8 de marco de 2024?"
-  "Bambam durou 1 minuto contra Popo?"
+    "Steve Jobs foi CEO da Pixar?",
+    "Ada Lovelace foi a primeira programadora?",
+    "Ayrton Senna venceu 3 mundiais?",
+    "A boneca barbie foi lancada em 59?",
+    "O homem pisou na lua em 69?",
+    "O tupi guarani e uma lingua oficial do BR?",
+    "O Brasil tem 3 liguas oficiais?",
+    "Cinderela a primeira princesa da Disney?",
+    "Akira Toriyama faleceu dia 8 de marco de 2024?",
+    "Bambam durou 1 minuto contra Popo?",
 };
 bool hardAnswers[10]{
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  false,
-  false,
-  false,
-  false,
-}
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+};
 
-String finalQuestion[3]{
-  "Chico moedas e um homem integro?"
-  "Corinthians e o melhor time de todos?"
-  "Este projeto merece uma nota 10?"
-}
-bool finalAnswer[3]{
-  true,
-  true,
-  true,
-}
+String finalQuestions[3]{
+    "Chico moedas e um homem integro?",
+    "Corinthians e o melhor time de todos?",
+    "Este projeto merece uma nota 10?",
+};
+
+bool finalAnswers[3]{
+    true,
+    true,
+    true,
+};
+
 bool gameActive = false;
 int questionIndex = 0;
 int skipsRemaining = 3;
@@ -104,19 +112,20 @@ bool isQuestionFullyDisplayed = false;
 unsigned long lastScrollTime = 0;
 bool isScrolling = false;
 int scrollPosition = 0; // Nova variável para controlar a posição da rolagem
-String question;
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);  // btn 1
-  pinMode(8, INPUT_PULLUP);  // btn 2
+  pinMode(10, INPUT_PULLUP); // btn 2
   pinMode(9, INPUT_PULLUP);  // btn 3
-  pinMode(10, INPUT_PULLUP); // btn 4
+  pinMode(8, INPUT_PULLUP);  // btn 4
   pinMode(7, OUTPUT);        // green
   pinMode(13, OUTPUT);       // red
   lcd.begin(16, 2);
   lcd.print("Bem vindo!");
-  attachInterrupt(digitalPinToInterrupt(2), start, FALLING);
+  String questions[] = {"Pergunta 1", "Pergunta 2", "Pergunta 3"};
+  attachInterrupt(digitalPinToInterrupt(2), start, RISING);
 }
 
 void loop()
@@ -153,7 +162,7 @@ void loop()
     {
       showOptionsAndScore();
     }
-  }  
+  }
 }
 
 void start()
@@ -166,46 +175,49 @@ void start()
 
 void nextQuestion()
 {
-  if (correctAnswer >= 5)
-  {
-    correctAnswer = 0;
-    currentLevel++;
-    if (currentLevel > 3)
-    {
-      endGame(true);
-      return;
-    }
-  }
-  questionIndex = random(0, 10);
-  currentLevel = 0;
 
-  switch (currentLevel)
+  currentLevel = 0;
+  questionIndex = 0; // ou sua lógica para escolher o índice
+
+  // Exibir a pergunta
+  displayQuestion(currentLevel, questionIndex);
+  // delay(2000);
+  // showOptionsAndScore();
+}
+
+void displayQuestion(int level, int index)
+{
+  String question;
+  if (level == 0)
   {
-  case 0:
-    question = easyQuestions[questionIndex];
-    break;
-  case 1:
-    question = mediumQuestions[questionIndex];
-    break;
-  case 2:
-    question = hardQuestions[questionIndex];
-    break;
-  case 3:
-    question = finalQuestions[random(0, 3)];
-    break;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    char buffer[50];
+    strcpy_P(buffer, (char *)pgm_read_word(&(easyQuestions[2]))); // Copia a string da memória de programa para o buffer
+    Serial.println(buffer);
+    lcd.print(buffer);
   }
-  lcd.clear();
-  lcd.print(question);
-  isQuestionFullyDisplayed = question.length() <= 16;
-  if (!isQuestionFullyDisplayed)
+  else if (level == 1)
   {
-    lastScrollTime = millis();
-    isScrolling = true;
-    scrollPosition = 0;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(mediumQuestions[index]);
   }
-  else
+  else if (level == 2)
   {
-    showOptionsAndScore(); // Chame aqui se a pergunta couber na tela sem rolagem
+    lcd.print(hardQuestions[index]);
+  }
+  else if (level == 3)
+  {
+    lcd.print(finalQuestions[index]);
+    int lengthOfQuestion = finalQuestions[index].length();
+    int scrollTimes = lengthOfQuestion > 16 ? lengthOfQuestion - 15 : 0;
+
+    for (int positionCounter = 0; positionCounter < scrollTimes; positionCounter++)
+    {
+      lcd.scrollDisplayLeft();
+      delay(3000); // Ajuste este delay conforme necessário
+    }
   }
 }
 
@@ -278,4 +290,29 @@ void endGame(bool won)
 void playSound(int soundType)
 {
   // soundType: 1 para acerto, 2 para pular, 3 para erro, 4 para vitória
+}
+
+void blinkWarning()
+{
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+}
+
+void skipQuestion()
+{
+  skipsRemaining--;
+  nextQuestion();
+  // Tocar som de pular (usando pino de som)
+}
+
+void showOptionsAndScore()
+{
+  // Mostra "SIM", "NÃO" e a pontuação
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("SIM      NAO");
+  lcd.setCursor(0, 1);
+  lcd.print("Score: ");
+  lcd.print(score);
 }
